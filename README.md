28e635fb8cb22576c0e8050a515594eecb46a228
soumyadip-cy <207708452+soumyadip-cy@users.noreply.github.com>
2025-06-04 00:01:10 +0530

    Added some initial tests and modified README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# **journalApp(In-Progress) | Backend project with Spring Boot and Mongo DB**
+# **journalApp | Backend project with Spring Boot and Mongo DB**
 
 Journal App is a **backend project** made using **Spring Boot** and **Mongo DB**.
 > GitHub link for this app is [journalApp](https://github.com/soumyadip-cy/journalApp)
@@ -7,4 +7,11 @@ Journal App is a **backend project** made using **Spring Boot** and **Mongo DB**
 
 ### Features
 
-1. 
+1. The project follows a Model-View-Controller (MVC) architecture, with components organized into distinct layers:
+   - Controller: Exposes the REST APIs (the presentation layer).
+   - Service: Encapsulates the business logic (the application/business logic layer).
+   - Repository: Facilitates and extends database querying (the data access/persistence layer).
+   - Entity: Holds the Plain Old Java Objects (POJOs) that map to database documents (part of the domain layer).
+   - Security: Stores security configurations (often a cross-cutting concern or part of an infrastructure layer). 
+
+2. The project utilizes MongoDB as its database and Spring Security for authentication. JUnit is used for automated unit and integration testing.
--- src/test/java/com/soumyadip_cy/journalApp/service/UserServiceTests.java
+++ src/test/java/com/soumyadip_cy/journalApp/service/UserServiceTests.java
@@ -1,10 +1,16 @@
 package com.soumyadip_cy.journalApp.service;
 
+import com.soumyadip_cy.journalApp.entity.User;
 import com.soumyadip_cy.journalApp.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 public class UserServiceTests {
@@ -15,8 +21,24 @@ public class UserServiceTests {
     @Autowired
     private UserRepository userRepository;
 
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "batman",
+            "superman",
+            "wonderwoman",
+    })
+    public void testFindByUserNameMultiple(String userName) {
+        Optional<User> user = userRepository.findByUserName(userName);
+        Assertions.assertNotNull(user.orElse(null), "Failed for: "+userName);
+    }
+
     @Test
     public void testFindByUserName() {
-        Assertions.assertNotNull(userService.findByUserName("batman").get());
+        String userName = "lois lane";
+        Optional<User> user = userService.findByUserName(userName);
+        Assertions.assertNull(user.isPresent() ? user.get() : null, "Failed for: "+userName);
     }
+
+    
+
 }
